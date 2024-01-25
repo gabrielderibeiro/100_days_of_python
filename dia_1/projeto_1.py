d857459697204f36c97e1504422520cc08dc451c
Gabriel <gabrielderibeiroo@gmail.com>
2024-01-25 07:53:34 -0300

    Dia 1 de python

---
+++ dia_1/projeto_1.py
@@ -0,0 +1,6 @@
+print('Welcome to the band name generator')
+city_name = input("What's name of city you grew up in? ")
+print(city_name)
+pet_name = input("What's your pet's name? ")
+print(pet_name)
+print(f'Your band name could be {city_name} {pet_name}')
